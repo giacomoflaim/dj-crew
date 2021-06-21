ebc9f70667955895bf646fc20bbb2e7b2ce9f419
Giacomo <giacomo.flaim.91@gmail.com>
2021-06-21 10:33:34 +0200

    first

---
+++ js/my_js.js
@@ -0,0 +1,20 @@
+
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() >900 && $(this).scrollTop() <1800) { 
+        //change yes to no
+        $('h3').html('data fusion');
+    } else if ($(this).scrollTop() >1800 && $(this).scrollTop() <2600) { 
+        //change yes to no
+        $('h3').html('uno scenario reale');
+    } else if ($(this).scrollTop() >2600 && $(this).scrollTop() <3500) { 
+        //change yes to no
+        $('h3').html('esplora la piattaforma');
+    } else {
+        //set h1 text to yes
+        $('h3').html('scrolla per continuare');
+    }
+});
+
+
+
