0a1ca19b54ad63198664f815f65ef117f4458865
Giacomo <giacomo.flaim.91@gmail.com>
2021-06-23 12:27:25 +0200

    update

--- js/my_js.js
+++ js/my_js.js
@@ -18,3 +18,7 @@ $(window).scroll(function () {
 
 
 
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
